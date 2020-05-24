78675ebf1711680ab4535a3f3c58a4b06a72095c
NovaisGabriel <gabrieel.novais@gmail.com>
2020-05-24 10:46:40 -0300

    Add jsx files

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,10 +3,15 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDom.render(
     <div>
         <Primeiro></Primeiro>
+        <ComParametro 
+            titulo="Segundo Componente"
+            subtitulo = "Muito Legal!">
+        </ComParametro>
     </div>,
     document.getElementById('root')
     )
